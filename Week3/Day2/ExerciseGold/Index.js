b3d05205caa0bd6426000e32ef1085c684e9dfa6
ehud <ehudmi12@gmail.com>
2022-03-29 18:05:58 +0300

    week 3 day 2 all exercises improved

--- Week3/Day2/ExerciseGold/Index.js
+++ Week3/Day2/ExerciseGold/Index.js
@@ -24,6 +24,19 @@ console.log(str3);
 let num1 = prompt("Pick the first number");
 console.log(typeof num1);
 let num2 = prompt("Pick the second number");
-// let oper1= prompt("Enter the operator")
-sum = Number(num1) + Number(num2);
-alert("your number is " + sum);
+let operator1 = prompt("Enter the operator either +   or  -  or  * or / ");
+if (operator1 == "+") {
+  sum = Number(num1) + Number(num2);
+  alert("your number is " + sum);
+} else if (operator1 == "-") {
+  sub = Number(num1) - Number(num2);
+  alert("your number is " + sub);
+}
+if (operator1 == "*") {
+  mult = Number(num1) * Number(num2);
+  alert("your number is " + mult);
+}
+if (operator1 == "/") {
+  div = Number(num1) / Number(num2);
+  alert("your number is " + div);
+}
