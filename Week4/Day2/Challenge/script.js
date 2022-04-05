8780a785e30fc7c77442c3d1503c25adb18e3062
ehud <ehudmi12@gmail.com>
2022-04-05 09:46:59 +0300

    week 4 day 2 challenge

--- Week4/Day2/Challenge/script.js
+++ Week4/Day2/Challenge/script.js
@@ -1,5 +1,44 @@
 // Exercise 1
 
-// Exercise 2
+let wordArray = prompt(`Give me your wise words`);
 
-// Exercise 3
+wordArray = wordArray.split(" ");
+console.log(wordArray);
+
+let longestWord = 0;
+let starArray = [];
+// function countLetters() {
+for (let i in wordArray) {
+  if (wordArray[i].length > longestWord) {
+    longestWord = wordArray[i].length;
+  }
+}
+// }
+
+// function fillArray() {
+// starArray[0] = "*".repeat(longestWord + 2);
+for (let i in wordArray) {
+  if (wordArray[i].length == longestWord) {
+    starArray[i] = "*" + wordArray[i] + "*";
+  } else if (wordArray[i].length % 2 == 0) {
+    starArray[i] =
+      "*" +
+      " ".repeat((longestWord - wordArray[i].length) / 2) +
+      wordArray[i] +
+      " ".repeat((longestWord - wordArray[i].length) / 2) +
+      "*";
+  } else {
+    starArray[i] =
+      "*" +
+      " ".repeat((longestWord - wordArray[i].length) / 2) +
+      wordArray[i] +
+      " ".repeat((longestWord - wordArray[i].length) / 2 + 1) +
+      "*";
+  }
+}
+// }
+starArray.unshift("*".repeat(longestWord + 2));
+starArray[starArray.length] = "*".repeat(longestWord + 2);
+for (let i in starArray) {
+  console.log(starArray[i]);
+}
