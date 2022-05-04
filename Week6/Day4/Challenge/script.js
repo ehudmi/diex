1cc3d87d299fd83316e8127ebaaf724a61244d89
ehud <ehudmi12@gmail.com>
2022-05-04 19:00:40 +0300

    Week 6 day 4 Challenge

--- Week6/Day4/Challenge/script.js
+++ Week6/Day4/Challenge/script.js
@@ -1,12 +1,57 @@
-// Challenge - True or False
+// Daily Challenge: Groceries
 
-let testArray = [1, 2, 3, 4];
+let client = "John";
 
-const allTruthy = (arr) =>
-  arr.every((val) =>
-    (val === Number(val) && val != 0) || (val === String(val) && val != "")
-      ? true
-      : false
-  );
+let groceries = {
+  fruits: ["pear", "apple", "banana"],
+  vegetables: ["tomatoes", "cucumber", "salad"],
+  totalPrice: "20$",
+  other: {
+    payed: true,
+    meansOfPayment: ["cash", "creditCard"],
+  },
+};
 
-console.log(allTruthy(testArray));
+// Hint: To do this daily challenge, take a look at today’s lesson Pass By Value & Pass By Reference
+
+// Create an arrow function named displayGroceries, that console.logs the 3 fruits from the groceries object.
+// Use the forEach method.
+
+// Create another arrow function named cloneGroceries.
+// In the function, create a variable named user that is a copy of the client variable.
+// (Tip : make the user variable equal to the client variable)
+// Change the client variable to “Betty”. Will we also see this modification in the user variable ? Why ?
+// In the function, create a variable named shopping that is a copy of the groceries object.
+// (Tip : make the shopping variable equal to the groceries variable)
+// Change the value of the totalPrice key to 35$. Will we also see this modification in the shopping object ? Why ?
+// Change the value of the payed key to false. Will we also see this modification in the shopping object ? Why ?
+
+// Invoke the cloneGroceries function.
+
+const displayGroceries = () => {
+  groceries.fruits.forEach(function (value) {
+    console.log(value);
+  });
+  // groceries.forEach(function () {
+  //   console.log(groceries.fruits);
+  // });
+};
+
+displayGroceries();
+
+const cloneGroceries = () => {
+  let user = client;
+  client = "Betty";
+  //The user will not change - client is defined as "Betty" after the variable user is defined as "John"
+  let shopping = groceries;
+  groceries.totalPrice = "35$";
+  //The shopping object will also be modified because they both refer to the same object
+  groceries.other.payed = false;
+  //The shopping object will also be modified because they both refer to the same object
+  console.log(user);
+  console.log(client);
+  console.log(groceries);
+  console.log(shopping);
+};
+
+cloneGroceries();
