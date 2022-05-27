ecfe4d31eb6e163ed0d2f04d0d5e27796d59d23f
ehud <ehudmi12@gmail.com>
2022-05-27 13:31:05 +0300

    Week 9 day 4 Ninja done

--- Week9/Day4/ExerciseGold/script.js
+++ Week9/Day4/ExerciseGold/script.js
@@ -1,70 +1,105 @@
 // Exercise 1: Analyze #2
 // Instructions
 
-let resolveAfter2Seconds = function () {
-  console.log("starting slow promise");
-  return new Promise((resolve) => {
-    setTimeout(function () {
-      resolve("slow");
-      console.log("slow promise is done");
-    }, 2000);
-  });
-};
+// let resolveAfter2Seconds = function () {
+//   console.log("starting slow promise");
+//   return new Promise((resolve) => {
+//     setTimeout(function () {
+//       resolve("slow");
+//       console.log("slow promise is done");
+//     }, 2000);
+//   });
+// };
 
-let resolveAfter1Second = function () {
-  console.log("starting fast promise");
-  return new Promise((resolve) => {
-    setTimeout(function () {
-      resolve("fast");
-      console.log("fast promise is done");
-    }, 1000);
-  });
-};
+// let resolveAfter1Second = function () {
+//   console.log("starting fast promise");
+//   return new Promise((resolve) => {
+//     setTimeout(function () {
+//       resolve("fast");
+//       console.log("fast promise is done");
+//     }, 1000);
+//   });
+// };
 
-let sequentialStart = async function () {
-  console.log("==SEQUENTIAL START==");
-  const slow = await resolveAfter2Seconds();
-  console.log(slow);
-  const fast = await resolveAfter1Second();
-  console.log(fast);
-};
+// let sequentialStart = async function () {
+//   console.log("==SEQUENTIAL START==");
+//   const slow = await resolveAfter2Seconds();
+//   console.log(slow);
+//   const fast = await resolveAfter1Second();
+//   console.log(fast);
+// };
 
-sequentialStart();
+// sequentialStart();
 
 // Analyze the code provided above before executing it - what will be the outcome?
+// Predicted:
+// "==SEQUENTIAL START=="
+// "starting fast promise"
+// "fast"
+// "fast promise is done"
+// "starting slow promise"
+// "slow"
+// "slow promise is done"
+
+// Actual:
+// ==SEQUENTIAL START==
+// script.js:5 starting slow promise
+// script.js:9 slow promise is done
+// script.js:27 slow
+// script.js:15 starting fast promise
+// script.js:19 fast promise is done
+// script.js:29 fast
 
 // Exercise 2: Analyze #3
 // Instructions
 
-let resolveAfter2Seconds = function () {
-  console.log("starting slow promise");
-  return new Promise((resolve) => {
-    setTimeout(function () {
-      resolve("slow");
-      console.log("slow promise is done");
-    }, 2000);
-  });
-};
+// let resolveAfter2Seconds = function () {
+//   console.log("starting slow promise");
+//   return new Promise((resolve) => {
+//     setTimeout(function () {
+//       resolve("slow");
+//       console.log("slow promise is done");
+//     }, 2000);
+//   });
+// };
 
-let resolveAfter1Second = function () {
-  console.log("starting fast promise");
-  return new Promise((resolve) => {
-    setTimeout(function () {
-      resolve("fast");
-      console.log("fast promise is done");
-    }, 1000);
-  });
-};
+// let resolveAfter1Second = function () {
+//   console.log("starting fast promise");
+//   return new Promise((resolve) => {
+//     setTimeout(function () {
+//       resolve("fast");
+//       console.log("fast promise is done");
+//     }, 1000);
+//   });
+// };
 
-let concurrentStart = async function () {
-  console.log("==CONCURRENT START with await==");
-  const slow = resolveAfter2Seconds();
-  const fast = resolveAfter1Second();
-  console.log(await slow);
-  console.log(await fast);
-};
+// let concurrentStart = async function () {
+//   console.log("==CONCURRENT START with await==");
+//   const slow = resolveAfter2Seconds();
+//   const fast = resolveAfter1Second();
+//   console.log(await slow);
+//   console.log(await fast);
+// };
 
-setTimeout(concurrentStart, 4000);
+// setTimeout(concurrentStart, 4000);
+
+// Predicted:
+// "==CONCURRENT START with await=="    //Starts after 4 seconds
+// "starting slow promise"
+// "slow promise is done"
+// "slow"
+// "starting fast promise"
+// "fast promise is done"
+// "fast"
+
+// Actual:
+// ==CONCURRENT START with await==
+// script.js:56 starting slow promise
+// script.js:66 starting fast promise
+// script.js:70 fast promise is done
+// script.js:60 slow promise is done
+// script.js:79 slow
+// script.js:80 fast
 
 // Analyze the code provided above before executing it - what will be the outcome?
 
@@ -78,16 +113,31 @@ const urls = [
   "https://jsonplaceholder.typicode.com/albums",
 ];
 
-const getData = async function () {
+// const getData = async function () {
+//   const [users, posts, albums] = await Promise.all(
+//     urls.map((url) => fetch(url).then((resp) => resp.json()))
+//   );
+//   console.log("users", users);
+//   console.log("posta", posts);
+//   console.log("albums", albums);
+// };
+
+// getData();
+
+const getDataAwait = async function () {
   const [users, posts, albums] = await Promise.all(
-    urls.map((url) => fetch(url).then((resp) => resp.json()))
+    urls.map(async (url) => {
+      let res = await fetch(url);
+      let resJson = await res.json();
+      return resJson;
+    })
   );
   console.log("users", users);
   console.log("posta", posts);
   console.log("albums", albums);
 };
 
-getData();
+getDataAwait();
 
 // Modify the function above. Add async await in place of the following line:
 // fetch(url).then(resp => resp.json())
--- Week9/Day4/ExerciseNinja/script.js
+++ Week9/Day4/ExerciseNinja/script.js
@@ -2,81 +2,132 @@
 // Instructions
 // Add a try catch block to “Exercise XP Gold” in order to catch any errors.
 // To test the catch, modify one of the urls. The catch should console.log ‘ooooooops’.
+const urls = [
+  "https://jsonplaceholder.typicode.com/users",
+  "https://jsonplaceholder.typicode.com/posts",
+  "https://jsonplaceholder.typicode.com/albums",
+];
+
+const getDataAwait = async function () {
+  try {
+    const [users, posts, albums] = await Promise.all(
+      urls.map(async (url) => {
+        let res = await fetch(url);
+        let resJson = await res.json();
+        return resJson;
+      })
+    );
+    console.log("users", users);
+    console.log("posta", posts);
+    console.log("albums", albums);
+  } catch (err) {
+    console.log("ooooooops");
+  }
+};
+
+getDataAwait();
 
 // Exercise 2 : Analyze #4
 // Instructions
 
-let resolveAfter2Seconds = function () {
-  console.log("starting slow promise");
-  return new Promise((resolve) => {
-    setTimeout(function () {
-      resolve("slow");
-      console.log("slow promise is done");
-    }, 2000);
-  });
-};
+// let resolveAfter2Seconds = function () {
+//   console.log("starting slow promise");
+//   return new Promise((resolve) => {
+//     setTimeout(function () {
+//       resolve("slow");
+//       console.log("slow promise is done");
+//     }, 2000);
+//   });
+// };
 
-let resolveAfter1Second = function () {
-  console.log("starting fast promise");
-  return new Promise((resolve) => {
-    setTimeout(function () {
-      resolve("fast");
-      console.log("fast promise is done");
-    }, 1000);
-  });
-};
+// let resolveAfter1Second = function () {
+//   console.log("starting fast promise");
+//   return new Promise((resolve) => {
+//     setTimeout(function () {
+//       resolve("fast");
+//       console.log("fast promise is done");
+//     }, 1000);
+//   });
+// };
 
 //The Promise.all() method returns a single Promise that fulfills when all of the promises passed as an iterable have been fulfilled.
 
-let concurrentPromise = function () {
-  console.log("==CONCURRENT START with Promise.all==");
-  return Promise.all([resolveAfter2Seconds(), resolveAfter1Second()]).then(
-    (messages) => {
-      console.log(messages[0]);
-      console.log(messages[1]);
-    }
-  );
-};
+// let concurrentPromise = function () {
+//   console.log("==CONCURRENT START with Promise.all==");
+//   return Promise.all([resolveAfter2Seconds(), resolveAfter1Second()]).then(
+//     (messages) => {
+//       console.log(messages[0]);
+//       console.log(messages[1]);
+//     }
+//   );
+// };
 
-setTimeout(concurrentPromise, 1000);
+// setTimeout(concurrentPromise, 1000);
 
 // Analyze the code provided above what will be the outcome?
+// Predicted:
+// "==CONCURRENT START with Promise.all=="
+// "starting slow promise"
+// "slow promise is done"
+// "slow"
+// "starting fast promise"
+// "fast promise is done"
+// "fast"
+
+// Actual:
+// ==CONCURRENT START with Promise.all==
+// script.js:34 starting slow promise
+// script.js:44 starting fast promise
+// script.js:48 fast promise is done
+// script.js:38 slow promise is done
+// script.js:59 slow
+// script.js:60 fast
 
 // Exercise 3 : Analyze #5
 // Instructions
 
-let resolveAfter2Seconds = function () {
-  console.log("starting slow promise");
-  return new Promise((resolve) => {
-    setTimeout(function () {
-      resolve("slow");
-      console.log("slow promise is done");
-    }, 2000);
-  });
-};
+// let resolveAfter2Seconds = function () {
+//   console.log("starting slow promise");
+//   return new Promise((resolve) => {
+//     setTimeout(function () {
+//       resolve("slow");
+//       console.log("slow promise is done");
+//     }, 2000);
+//   });
+// };
 
-let resolveAfter1Second = function () {
-  console.log("starting fast promise");
-  return new Promise((resolve) => {
-    setTimeout(function () {
-      resolve("fast");
-      console.log("fast promise is done");
-    }, 1000);
-  });
-};
+// let resolveAfter1Second = function () {
+//   console.log("starting fast promise");
+//   return new Promise((resolve) => {
+//     setTimeout(function () {
+//       resolve("fast");
+//       console.log("fast promise is done");
+//     }, 1000);
+//   });
+// };
 
-let parallel = async function () {
-  console.log("==PARALLEL with await Promise.all==");
-  // Start 2 "jobs" in parallel and wait for both of them to complete
-  await Promise.all([
-    (async () => console.log(await resolveAfter2Seconds()))(),
-    (async () => console.log(await resolveAfter1Second()))(),
-  ]);
-};
+// let parallel = async function () {
+//   console.log("==PARALLEL with await Promise.all==");
+//   // Start 2 "jobs" in parallel and wait for both of them to complete
+//   await Promise.all([
+//     (async () => console.log(await resolveAfter2Seconds()))(),
+//     (async () => console.log(await resolveAfter1Second()))(),
+//   ]);
+// };
 
-setTimeout(parallel, 5000);
+// setTimeout(parallel, 5000);
 
 // Analyze the code provided above what will be the outcome?
+// Predicted:
+// "==PARALLEL with await Promise.all==" //After 5 seconds
+// "starting slow promise"
+// "starting fast promise"
+// "fast promise is done"
+// "fast"
+// "slow promise is done"
+// "slow"
+
+// Actual: Same as predicted (finally)
 
 // Exercise 4 : Analyze #6
 // Instructions
@@ -112,3 +163,13 @@ let parallelPromise = function () {
 setTimeout(parallelPromise, 13000);
 
 // Analyze the code provided above what will be the outcome?
+// Predicted:
+// "==PARALLEL with Promise.then==" // After 13 seconds
+// "starting slow promise"
+// "starting fast promise"
+// "fast promise is done"
+// "fast"
+// "slow promise is done"
+// "slow"
+
+// Actual: Same as predicted (finally)
