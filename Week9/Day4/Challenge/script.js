95fae693ade316bc7edb5e285b7d275d4b5fb096
ehud <ehudmi12@gmail.com>
2022-05-27 20:19:25 +0300

    Week 9 Day 4 Challenge

--- Week9/Day4/Challenge/script.js
+++ Week9/Day4/Challenge/script.js
@@ -6,13 +6,20 @@
 
 // Example
 
-const promise1 = Promise.resolve(3);
-const promise2 = 42;
-const promise3 = new Promise((resolve, reject) => {
-  setTimeout(resolve, 3000, "foo");
-});
+// const promise1 = Promise.resolve(3);
+// const promise2 = 42;
+// const promise3 = new Promise((resolve, reject) => {
+//   setTimeout(resolve, 3000, "foo");
+// });
 
-// expected output: Array [3, 42, "foo"]
+const promiseArray = [promise1, promise2, promise3];
+
+const resolvePromise = () =>
+  Promise.all(promiseArray)
+    .then((result) => console.log(result))
+    .catch((error) => console.log(error));
+
+resolvePromise();
 
 // 2nd Challenge
 // Instructions
@@ -34,3 +41,56 @@ const promise3 = new Promise((resolve, reject) => {
 // New York
 // Latitude: 40.730610
 // Longitude: -73.935242
+
+const myForm = document.forms[0];
+
+// collect the data on form submit
+
+myForm.addEventListener("submit", (e) => {
+  e.preventDefault();
+
+  // Get the values into an object
+
+  const formData = new FormData(e.target);
+  const locData = Object.fromEntries(formData);
+
+  // Configure my request
+
+  const urls = [];
+
+  let myUrl = "https://api.sunrise-sunset.org/json?";
+  let params1 = `lat=${locData.first_city_lat}&lng=${locData.first_city_long}`;
+  let params2 = `lat=${locData.second_city_lat}&lng=${locData.second_city_long}`;
+
+  urls.push(myUrl + params1, myUrl + params2);
+
+  // create the async function to get the data
+
+  const getDataAwait = async function () {
+    try {
+      const [sunrise1, sunrise2] = await Promise.all(
+        urls.map(async (url) => {
+          let res = await fetch(url);
+          let resJson = await res.json();
+          return resJson;
+        })
+      );
+      let sunrise1Box = document.createElement("div");
+      let sunrise1Text = document.createTextNode(
+        `First location sunrise time is ${sunrise1.results.sunrise}`
+      );
+      document.querySelector("div").appendChild(sunrise1Box);
+      sunrise1Box.appendChild(sunrise1Text);
+      let sunrise2Box = document.createElement("div");
+      let sunrise2Text = document.createTextNode(
+        `Second location sunrise time is ${sunrise2.results.sunrise}`
+      );
+      document.querySelector("div").appendChild(sunrise2Box);
+      sunrise2Box.appendChild(sunrise2Text);
+    } catch (err) {
+      console.log("ooooooops", err);
+    }
+  };
+
+  getDataAwait();
+});
