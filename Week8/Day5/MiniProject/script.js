dbccf96e6fcf9e982dfd9467fb05afef6087a19f
ehud <ehudmi12@gmail.com>
2022-05-31 19:45:48 +0300

    weather app all done

--- Week8/Day5/MiniProject/script.js
+++ Week8/Day5/MiniProject/script.js
@@ -1,59 +1,3 @@
-// Mini Project : Weather App
-// What You Will Learn
-// Work with an API
-// Perform an AJAX request
-
-// What You Will Create
-// Build a Weather App
-
-// This project is going to teach you how to build a weather application with JS, HTML, and CSS.
-// You will use the openweathermap API for fetching weather information,
-// which is a great opportunity for you to learn how to interact with APIs, another great thing you can do with javascript.
-// Instructions
-// What is OpenWeather?
-
-// In our case, we’re going to use OpenWeatherMap, one of the most popular free choices.
-// OpenWeather describes itself as a group of IT experts and data scientists that does deep weather data science since 2014.
-// For each point on Earth, OpenWeather provides reliable historical, current and forecasted weather data via light-speed APIs.
-
-// You are going to use the OpenWeatherMap API, with this API Key: 6bc236fa8bd5e7e03f83fd8cea3eac74.
-
-// Part I - Weather App
-// Create an HTML, CSS and JS file.
-
-// Add a form in the HTML file to allow the user to type in a city.
-// As soon as he clicks on the “Submit” button. You should retrieve the city,
-// then retrieve the city information from the API and finally display the result in a nice card, like the example above.
-// Use the Current Weather Data to get the information about a specific city.
-
-// If the user enters a correct city, you should display in the card:
-// the name of the city, and the name of the country where the city is located
-// the weather (ie. “clear”, “sunny” ect…)
-// the icon of the weather. Check out this part of the documentation
-// the temperature (Temperature in Kelvin is used by default), the humidity
-
-// Warn the user, if he didn’t enter a correct city
-
-// Either way, as soon as the user submits his answer, reset the form input (ie. make it empty).
-
-// in the JS file, the information about each city needs to be saved, in order to display all the previous searches.
-// How should you save the information ? Which data type should you use ?
-
-// Part II - Weather App Improved
-// Add a button at the end of the HTML, that displays the temperature in another unit :
-// Fahrenheit or Celsius. How should you change the API request ?
-
-// 2. Display the wind info, the sunrise and sunset time in the city.
-
-// 3. Make the website responsive
-
-// Part III - Bonus
-// Retrieve by default the user’s location (latitute and longitude), and display the location’s weather.
-
-// Add an Error popup if the searched city could not be found.
-
-// Add a “X” next to each card, in order to delete it from the DOM.
-
 // the constants for the app
 
 const myForm = document.forms[0];
@@ -96,7 +40,6 @@ const getLocation = () => {
           document
             .querySelector("#current-location")
             .appendChild(currentWeatherIcon);
-          console.log(firstXhr.response);
         }
       };
     });
@@ -114,27 +57,28 @@ const convertUnixTime = (unix_timestamp) => {
   let hours = date.getHours();
   let minutes = "0" + date.getMinutes();
   let formattedTime = hours + ":" + minutes.substr(-2);
-  console.log(formattedTime);
   return formattedTime;
 };
 
 const convertTemp = () => {
   if (document.querySelector("#no").checked) {
-    document.querySelectorAll(".card-temp").forEach(
-      // (element) => element.innerHTML == (element.innerHTML * 9) / 5 + 32
-      (element) => {
-        if (element.innerHTML.includes("F"))
-          element.innerHTML == (element.innerHTML * 9) / 5 + 32;
+    document.querySelectorAll(".card-temp").forEach((element) => {
+      if (element.innerHTML.includes("F")) {
+        element.innerHTML = `${(
+          (Number(element.innerHTML.slice(0, 5)) * 9) / 5 +
+          32
+        ).toFixed(2)}, °C`;
       }
-    );
+    });
   } else if (document.querySelector("#yes").checked) {
-    document.querySelectorAll(".card-temp").forEach(
-      // (element) => element.innerHTML == ((element.innerHTML - 32) * 5) / 9
-      (element) => {
-        if (element.innerHTML.includes("C"))
-          element.innerHTML == ((element.innerHTML - 32) * 5) / 9;
+    document.querySelectorAll(".card-temp").forEach((element) => {
+      if (element.innerHTML.includes("C")) {
+        element.innerHTML = `${(
+          ((Number(element.innerHTML.slice(0, 5)) - 32) * 5) /
+          9
+        ).toFixed(2)}, °F`;
       }
-    );
+    });
   }
 };
 
@@ -149,14 +93,12 @@ myForm.querySelector("#find").addEventListener("click", (e) => {
   xhr.open("GET", cityApiURL);
   xhr.responseType = "json";
   xhr.send();
-
   xhr.onload = () => {
     if (xhr.status != 200) {
       let errorModal = new bootstrap.Modal(
         document.querySelector("#error_modal")
       );
       errorModal.show();
-      console.log(errorModal);
     } else {
       // Create and populate the dataObject with response data
       const dataObject = {
@@ -181,12 +123,8 @@ myForm.querySelector("#find").addEventListener("click", (e) => {
       dataObject.temperature_fahrenheit =
         (dataObject.temperature_celsius * 9) / 5 + 32;
       dataObject.wind = xhr.response.wind;
-
       dataObject.sunrise = convertUnixTime(xhr.response.sys.sunrise);
       dataObject.sunset = convertUnixTime(xhr.response.sys.sunset);
-
-      console.log(dataObject);
-      console.log(xhr.response);
 
       // create the DOM elements after getting the response
 
@@ -238,5 +176,5 @@ myForm.querySelector("#find").addEventListener("click", (e) => {
   };
 });
 
-document.querySelector("#no").addEventListener("click", (e) => console.log(e));
-document.querySelector("#yes").addEventListener("click", (e) => console.log(e));
+document.querySelector("#no").addEventListener("click", convertTemp);
+document.querySelector("#yes").addEventListener("click", convertTemp);
