03357aa88cb92d71b5d8d9548e439146c04d6c2d
ehud <ehudmi12@gmail.com>
2022-06-05 14:22:37 +0300

    Week 10 Day 5 Challenge

--- Week10/Day5/Challenge/SQL_queries_all.sql
+++ Week10/Day5/Challenge/SQL_queries_all.sql
@@ -5,7 +5,17 @@
 
 -- There should be a one to many relationship between the orders table and the items table.
 -- An order can have many items, but an item can belong to only one order.
-
+CREATE TABLE items (item_id SERIAL PRIMARY KEY, description VARCHAR (255), price INTEGER);
+CREATE TABLE orders (order_id INTEGER,order_date DATE, item_id INTEGER, quantity INTEGER,
+FOREIGN KEY (item_id) REFERENCES items(item_id));
+INSERT INTO items (description,price) VALUES ('big lamp',300), ('small lamp',150),
+('big chair',800),('small chair',400),('big bed',2000),('small bed',1000);
+INSERT INTO orders (order_id,order_date,item_id,quantity) VALUES (1,'01/06/2020',1,4),(1,'01/06/2020',3,2),
+(1,'01/06/2020',4,1),(2,'02/06/2020',3,1),(2,'02/06/2020',2,2),(2,'02/06/2020',5,2);
 -- Display all the items.
+SELECT * FROM items;
 -- Display all the orders.
+SELECT * FROM orders;
 -- Use Inner Join to display the orders with their items.
+SELECT order_id,orders.item_id,items.description,quantity,quantity*items.price AS "Total Order Price" FROM orders 
+INNER JOIN items ON orders.item_id=items.item_id;
--- Week10/Day5/ExerciseGold/SQL_queries_all.sql
+++ Week10/Day5/ExerciseGold/SQL_queries_all.sql
@@ -13,24 +13,25 @@
 
 -- Aggregate Functions
 -- Write a query to find the number of jobs available in the employees table.
-
+SELECT COUNT(job_id) FROM employees ; 
 -- Write a query to get the number of employees working in each post.
-
+SELECT COUNT(employee_id) FROM employees GROUP BY job_id ; 
 -- Write a query to get the difference between the highest and lowest salaries.
-
+SELECT (MAX(salary)-MIN(salary)) FROM employees ; 
 -- Write a query to find a manager ID and the salary of the lowest-paid employee under that manager.
-
+SELECT MIN(salary) FROM employees WHERE manager_id=108 ; 
 -- Write a query to get the average salary for each post excluding programmer.
-
+SELECT jobs.job_title,AVG(salary) FROM employees INNER JOIN jobs ON employees.job_id=jobs.job_id
+WHERE jobs.job_title!='Programmer' GROUP BY jobs.job_id ; 
 -- Write a query to get the average salary for all departments that employ more than 10 employees.
 
 
 -- Alter Table Statement
 -- Write a SQL statement to change the name of the column “state_province” to “state” in the locations table,
 -- keeping the same data type and size.
-
+ALTER TABLE locations RENAME COLUMN state_province TO state;
 -- Write a SQL statement to add a primary key to the “location_id” column in the locations table.
-
+ALTER TABLE locations ADD PRIMARY KEY(location_id);
 
 -- Create Tables
 -- Some Review:
@@ -55,27 +56,31 @@
 -- Write a SQL statement to create a simple table “new_countries” including columns country_id and country_name.
 -- make sure that no duplicate data is added to the country_id column (which data type should you use for the column country_id ?)
 -- make sure that no countries except Italy, India and China will be entered in the table.
-
+CREATE TABLE new_countries (country_id INTEGER UNIQUE NOT NULL, country_name VARCHAR(100) NOT NULL
+CHECK (country_name='India' OR country_name='Italy' OR country_name='China'));
 -- Write a SQL statement to create a duplicate copy of the “new_countries” table including
 -- the structure and the data of the “new_countries” table.
-
+CREATE TABLE dupe_new_countries AS SELECT * FROM new_countries;
 -- Write a SQL statement to create a table named “new_jobs” including columns job_id, job_title, min_salary, max_salary
 -- make sure that the max_salary column won’t exceed 25000.
 -- make sure that job_title, min_salary and max_salary have the following default values:
 -- job_title is blank
 -- min_salary is 8000
 -- max_salary is NULL.
-
+CREATE TABLE new_jobs (job_id INTEGER UNIQUE NOT NULL, job_title VARCHAR(100) DEFAULT '',
+min_salary INTEGER DEFAULT 8000, max_salary INTEGER DEFAULT NULL CHECK (max_salary<=25000));
 -- Write an SQL statement to create a table called “new_employees” the table should include the following columns:
 -- employee_id, first_name, last_name, email, phone_number hire_date, job_id and salary.
 -- make sure that, the employee_id column does not contain any duplicate value at the time of insertion,
 -- make sure that the foreign key column job_id, references the column job_id in the “new_jobs” table.
-
+CREATE TABLE new_employees 	(employee_id INTEGER UNIQUE, first_name VARCHAR(50), last_name VARCHAR(50), email VARCHAR(50),
+phone_number VARCHAR(50), hire_date DATE, job_id INTEGER, salary INTEGER, FOREIGN KEY (job_id) REFERENCES new_jobs(job_id));
 -- Write a SQL statement to create a table called “new_job_history” the table should include the following columns:
 -- employee_id, start_date, end_date, job_id
 -- make sure that the foreign key employee_id references the column employee_id in the “new_employees” table.
 -- make sure that the foreign key job_id is equal to an id that exists in the “new_jobs” table.
-
+CREATE TABLE new_job_history (employee_id INTEGER , start_date DATE, end_date DATE, job_id INTEGER,
+FOREIGN KEY (employee_id) REFERENCES new_employees(employee_id), FOREIGN KEY (job_id) REFERENCES new_jobs(job_id) ON DELETE CASCADE);
 
 -- Insert
 -- Write a SQL statement to insert a record with your own value into the “new_countries” table.
