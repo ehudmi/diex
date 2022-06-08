140c8d949e3754ea998d042fd7ce899219a0e881
ehud <ehudmi12@gmail.com>
2022-06-08 18:48:58 +0300

    Week 11 Day 2 Challenge

--- Week11/Day2/Challenge/SQL_queries_all.sql
+++ Week11/Day2/Challenge/SQL_queries_all.sql
@@ -14,17 +14,34 @@
 -- John, Doe
 -- Jerome, Lalu
 -- Lea, Rive
-
+CREATE TABLE customer (id SERIAL , first_name VARCHAR(50), last_name VARCHAR(50) NOT NULL);
+CREATE TABLE customer_profile
+(id SERIAL, isLoggedIn BOOLEAN DEFAULT false, customer_id INTEGER);
+INSERT INTO customer (first_name, last_name) 
+VALUES ('John', 'Doe'), ('Jerome', 'Lalu'), ('Lea', 'Rive');
 
 -- 3.Insert those customer profiles, use subqueries
 -- John is loggedIn
 -- Jerome is not logged in
+INSERT INTO customer_profile (isLoggedIn,customer_id) 
+(SELECT 't',id FROM customer WHERE first_name ='John');
+
+INSERT INTO customer_profile (isLoggedIn,customer_id) 
+(SELECT 'f',id FROM customer WHERE first_name ='Jerome');
+
 
 -- Use the relevant types of Joins to display:
 -- The first_name of the LoggedIn customers
+SELECT first_name FROM customer cu
+INNER JOIN customer_profile cp ON cu.id=cp.customer_id
+WHERE cp.isLoggedIn='t';
 -- All the customers first_name and isLoggedIn columns - even the customers those who don’t have a profile.
+SELECT first_name, cp.isLoggedIn FROM customer cu
+LEFT JOIN customer_profile cp ON cu.id=cp.customer_id;
 -- The number of customers that are not LoggedIn
-
+SELECT COUNT(cu.id) FROM customer cu
+INNER JOIN customer_profile cp ON cu.id=cp.customer_id
+WHERE cp.isLoggedIn!='t';
 
 -- Part II:
 
@@ -43,7 +60,20 @@
 -- Lera, 11
 -- Patrick, 10
 -- Bob, 14
-
+CREATE TABLE Book (book_id SERIAL PRIMARY KEY, title VARCHAR(100) NOT NULL, author VARCHAR(255) NOT NULL);
+INSERT INTO Book (title, author)
+VALUES
+('Alice In Wonderland', 'Lewis Carroll'),
+('Harry Potter', 'J.K Rowling'),
+('To kill a mockingbird', 'Harper Lee');
+CREATE TABLE Student
+(student_id SERIAL PRIMARY KEY, name VARCHAR (100) NOT NULL UNIQUE, age INTEGER CHECK (age<=15));
+INSERT INTO Student (name, age)
+VALUES
+('John', 12),
+('Lera', 11),
+('Patrick', 10),
+('Bob', 14);
 -- Create a table named Library, with the columns :
 -- book_fk_id ON DELETE CASCADE ON UPDATE CASCADE
 -- student_id ON DELETE CASCADE ON UPDATE CASCADE
@@ -53,15 +83,53 @@
 -- book_fk_id is a Foreign Key representing the column book_id from the Book table
 -- student_fk_id is a Foreign Key representing the column student_id from the Student table
 -- The pair of Foreign Keys is the Primary Key of the Junction Table
-
+CREATE TABLE Library
+(book_fk_id INTEGER ,
+ student_id INTEGER ,
+ borrowed_date DATE,
+FOREIGN KEY (book_fk_id) REFERENCES book(book_id) ON DELETE CASCADE ON UPDATE CASCADE,
+FOREIGN KEY (student_id) REFERENCES student(student_id) ON DELETE CASCADE ON UPDATE CASCADE,
+PRIMARY KEY (book_fk_id,student_id));
 -- Add 4 records in the junction table, use subqueries.
 -- the student named John, borrowed the book Alice In Wonderland on the 15/02/2022
 -- the student named Bob, borrowed the book To kill a mockingbird on the 03/03/2021
 -- the student named Lera, borrowed the book Alice In Wonderland on the 23/05/2021
 -- the student named Bob, borrowed the book Harry Potter the on 12/08/2021
-
+INSERT INTO library (book_fk_id,
+					   student_id,
+					   borrowed_date)
+VALUES(
+(SELECT book_id FROM book WHERE title = 'Alice In Wonderland'),
+(SELECT student_id FROM student WHERE name ILIKE '%John%'),
+	'15/02/2022'
+),
+(
+(SELECT book_id FROM book WHERE title = 'To kill a mockingbird'),
+(SELECT student_id FROM student WHERE name ILIKE '%Bob%'),
+	'03/03/2021'
+),
+(
+(SELECT book_id FROM book WHERE title = 'Alice In Wonderland'),
+(SELECT student_id FROM student WHERE name ILIKE '%Lera%'),
+	'23/05/2021'
+),
+(
+(SELECT book_id FROM book WHERE title = 'Harry Potter'),
+(SELECT student_id FROM student WHERE name ILIKE '%Bob%'),
+	'12/08/2021'
+);
 -- Display the data
 -- Select all the columns from the junction table
+SELECT * FROM library;
 -- Select the name of the student and the title of the borrowed books
+SELECT s.name, b.title FROM library l
+INNER JOIN student s ON l.student_id=s.student_id
+INNER JOIN book b ON l.book_fk_id=b.book_id;
 -- Select the average age of the children, that borrowed the book Alice in Wonderland
+SELECT b.title, AVG(s.age) FROM library l
+INNER JOIN student s ON l.student_id=s.student_id
+INNER JOIN book b ON l.book_fk_id=b.book_id
+WHERE b.title='Alice In Wonderland'
+GROUP BY b.title;
 -- Delete a student from the Student table, what happened in the junction table ?
+-- Answer: The cascaded delete removed the row for that student from the library table
--- Week11/Day2/ExerciseGold/SQL_queries_all.sql
+++ Week11/Day2/ExerciseGold/SQL_queries_all.sql
@@ -1,12 +1,29 @@
 -- Exercise 1 : DVD Rentals
 -- Instructions
 -- Get a list of all rentals which are out (have not been returned). How do we identify these films in the database?
-
+SELECT f.film_id, title  
+FROM film f
+INNER JOIN inventory i ON f.film_id=i.film_id
+INNER JOIN rental r ON i.inventory_id=r.inventory_id
+WHERE 
+r.return_date IS NULL;
 -- Get a list of all customers who have not returned their rentals. Make sure to group your results.
-
+SELECT CONCAT(c.first_name,' ' ,c.last_name) AS "Customer Name", COUNT(f.film_id)
+FROM film f
+INNER JOIN inventory i ON f.film_id=i.film_id
+INNER JOIN rental r ON i.inventory_id=r.inventory_id
+INNER JOIN customer c ON r.customer_id=c.customer_id
+WHERE 
+r.return_date IS NULL
+GROUP BY c.customer_id
+ORDER bY COUNT(f.film_id) DESC;
 -- Get a list of all the Action films with Joe Swank.
--- Before you start, could there be a shortcut to getting this information? Maybe a view?
 
+-- Before you start, could there be a shortcut to getting this information? Maybe a view?
+SELECT fid, title, description, category, price, length, rating, actors
+	FROM public.film_list
+		WHERE actors ILIKE '%Joe Swank%'
+	AND category='Action';
 
 -- Exercise 2 – Happy Halloween
 -- Instructions
@@ -15,17 +32,40 @@
 -- until the zombies are destroyed by the armed forces. Prepare tables with the following data:
 
 -- How many stores there are, and in which city and country they are located.
-
+SELECT co.country, ci.city, COUNT(s.store_id) FROM store s
+INNER JOIN address a ON s.address_id=a.address_id
+INNER JOIN city ci ON a.city_id=ci.city_id
+INNER JOIN country co ON ci.country_id=co.country_id
+GROUP BY co.country, ci.city;
 -- How many hours of viewing time there are in total in each store – in other words,
 -- the sum of the length of every inventory item in each store.
-
+SELECT co.country, ci.city, SUM(f.length)/60 AS "View Hours" FROM film f
+INNER JOIN inventory i ON f.film_id=i.film_id
+INNER JOIN rental r ON i.inventory_id=r.inventory_id
+INNER JOIN store s ON i.store_id=s.store_id
+INNER JOIN address a ON s.address_id=a.address_id
+INNER JOIN city ci ON a.city_id=ci.city_id
+INNER JOIN country co ON ci.country_id=co.country_id
+WHERE return_date IS NOT NULL
+GROUP BY co.country, ci.city;
 -- Make sure to exclude any inventory items which are not yet returned.
 -- (Yes, even in the time of zombies there are people who do not return their DVDs)
 
 -- A list of all customers in the cities where the stores are located.
-
+SELECT ci.city, CONCAT(cu.first_name,' ',cu.last_name) AS "Customer" FROM customer cu
+INNER JOIN store s ON cu.store_id=s.store_id
+INNER JOIN address a ON s.address_id=a.address_id
+INNER JOIN city ci ON a.city_id=ci.city_id
+GROUP BY ci.city,cu.last_name,cu.first_name
+ORDER BY ci.city ASC;
 -- A list of all customers in the countries where the stores are located.
-
+SELECT co.country, CONCAT(cu.first_name,' ',cu.last_name) AS "Customer" FROM customer cu
+INNER JOIN store s ON cu.store_id=s.store_id
+INNER JOIN address a ON s.address_id=a.address_id
+INNER JOIN city ci ON a.city_id=ci.city_id
+INNER JOIN country co ON ci.country_id=co.country_id
+GROUP BY co.country,cu.last_name,cu.first_name
+ORDER BY co.COUNTRY ASC;
 -- Some people will be frightened by watching scary movies while zombies walk the streets.
 -- Create a ‘safe list’ of all movies which do not include the ‘Horror’ category,
 -- or contain the words ‘beast’, ‘monster’, ‘ghost’, ‘dead’, ‘zombie’, or ‘undead’ in their titles or descriptions…
