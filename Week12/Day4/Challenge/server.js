71d2203c0f7bf0bec492acd593729be69980be1b
ehud <ehudmi12@gmail.com>
2022-06-18 09:48:33 +0300

    Week 12 Day 4 Challenge

--- Week12/Day4/Challenge/server.js
+++ Week12/Day4/Challenge/server.js
@@ -1,12 +1,24 @@
 const express = require("express");
 const bodyParser = require("body-parser");
+const extractnumbers = require("extract-numbers");
+
 const app = express();
 
 app.use(express.static(__dirname + "/public"));
 
 app.use(bodyParser.urlencoded({ extended: true }));
 
-app.get("/aboutMe/:hobby", (req, res) => {});
+app.get("/aboutMe/:hobby", (req, res) => {
+  const hobbies = [
+    { id: 1, hobby: "reading" },
+    { id: 2, hobby: "tv" },
+    { id: 3, hobby: "porn" },
+  ];
+  let i = hobbies.findIndex((ob) => ob.hobby == req.params.hobby);
+  extractnumbers(req.params.hobby) == null
+    ? res.json(hobbies[i])
+    : res.status(404).send("These are not the droids");
+});
 
 app.get("/pic", (req, res) => {
   res.sendFile("C:/Github/diex/Week12/Day4/Challenge/public/image.html");
