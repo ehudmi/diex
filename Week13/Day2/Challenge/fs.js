fd62e27e881f7f732238b52e4c0a1ad51f451b5c
ehud <ehudmi12@gmail.com>
2022-06-24 09:08:41 +0300

    Week 13 day 2 challenge

--- Week13/Day2/Challenge/fs.js
+++ Week13/Day2/Challenge/fs.js
@@ -16,7 +16,7 @@ const countSteps = revSteps.reduce((acc, curr, index) => {
   let total = acc + curr;
   return total;
 }, 0);
-console.log(`Total Steps: ${countSteps + 1}`);
+console.log(`Total Steps: ${countSteps}`);
 
 let totSteps = 0;
 const countStepsBeyond0 = revSteps.reduce((acc, curr, index) => {
@@ -30,6 +30,43 @@ const countStepsBeyond0 = revSteps.reduce((acc, curr, index) => {
 console.log(
   `Total Steps: ${totSteps}\nFirst time in left side is in ${countStepsBeyond0} steps`
 );
+
+// const where_am_i_2 = () => {
+//   fs.readFile(path.join(__dirname, "RightLeft.txt"), (err, data) => {
+//     const direction = data.toString();
+//     const arr = direction.split("");
+
+//     let index = 0;
+//     let steps = 0;
+
+//     const x = arr.some((currentValue) => {
+//       if (currentValue === ">") {
+//         ++index;
+//       } else if (currentValue === "<") {
+//         --index;
+//       }
+//       steps++;
+//       return index < 0;
+//     });
+
+//     console.log(steps);
+
+// for(x of arr){
+//   if(x==='>'){
+//     steps++;
+//   }
+//   else if(x==='<'){
+//     steps--;
+//   }
+//   index++
+//   if(steps < 0) {
+//     break;
+//   }
+// }
+// console.log(index);
+//   });
+// };
+// where_am_i_2();
 
 app.listen(3000, () => {
   console.log("Server up and running on port 3000");
