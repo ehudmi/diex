ee1a607bf9d830ce8a646d5cd8501baff69cbd50
ehud <ehudmi12@gmail.com>
2023-03-29 20:02:53 +0300

    f

--- Week3/Day4/Challenge/script.js
+++ Week3/Day4/Challenge/script.js
@@ -3,8 +3,8 @@ let wordNot = sentence.indexOf("not");
 let wordBad = sentence.indexOf("bad");
 
 if (wordNot < wordBad) {
-  sentence = sentence.replace(sentence.slice(wordNot, wordBad + 3), "good");
-  console.log(sentence);
+	sentence = sentence.replace(sentence.slice(wordNot, wordBad + 3), "good");
+	console.log(sentence);
 } else {
-  console.log(sentence);
+	console.log(sentence);
 }
